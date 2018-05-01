53240497d201a7db80b3892178da4024d0a3a1a7
sbroadhurst <stepbro31@gmail.com>
2018-05-01 11:14:30 -0500

    changed next/prev buttons to mat-ui

--- src/components/Swimlane.js
+++ src/components/Swimlane.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Poster from './Poster'
 import Carousel from 'nuka-carousel'
+import '../boxes.css'
+import RaisedButton from 'material-ui/RaisedButton'
 
 class Swimlane extends React.Component {
   renderPopular() {
@@ -9,6 +11,12 @@ class Swimlane extends React.Component {
       <Carousel
         slideWidth={'150px'}
         renderBottomCenterControls={() => {}}
+        renderCenterLeftControls={({ previousSlide }) => (
+          <RaisedButton className="prev" onClick={previousSlide} label="PREV" />
+        )}
+        renderCenterRightControls={({ nextSlide }) => (
+          <RaisedButton className="next" onClick={nextSlide} label="NEXT" />
+        )}
         frameOverflow="show"
       >
         {posters.map((info, index) => {
